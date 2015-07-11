f8047c601a12b28ceca12422026c3b5058546d68
brodieaustin <brodie.austin@gmail.com>
2015-07-10 19:43:51 -0500

    added gulp-inline-source

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp'), 
+  sass = require('gulp-sass') ,
+  autoprefixer = require('gulp-autoprefixer'),
+  minifycss = require('gulp-minify-css'),
+  inlinesource = require('gulp-inline-source'),
+  browserSync = require('browser-sync')
+  //concat = require('gulp-concat'),
+  //uglify = require('gulp-uglify')
+
+var config = {
+    sass_path: 'src/assets/scss',
+    css_path: 'src/assets/css',
+    js_src_path: 'src/assets/js/src',
+    js_path: 'dist/assets/js',
+    bower_dir: 'bower_components'
+}
+
+gulp.task('sass', function () {
+    gulp.src(config.sass_path + '/*.scss')
+    .pipe(sass({ includePaths : [config.sass_path, config.bower_dir + '/normalize-scss/'] }))
+    .pipe(autoprefixer({browsers: ['last 2 versions'],cascade: false}))
+    .pipe(minifycss())
+    .pipe(gulp.dest(config.css_path))
+    .pipe(browserSync.reload({stream:true}))
+});
+
+gulp.task('inlinesource', function () {
+    return gulp.src('./src/*.html')
+        .pipe(inlinesource())
+        .pipe(gulp.dest('./dist'));
+});
+
+
+/*gulp.task('js', function(){
+  gulp.src([config.bower_dir + '/svg4everybody/svg4everybody.js', config.js_src_path + '/*.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.js_path))
+});*/
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'dist/'
+        },
+    });
+});
+
+gulp.task('browser-sync-reload', function(){
+  browserSync.reload();
+})
+
+//watch sass for changes
+gulp.task('watch', function() {
+  gulp.watch('src/assets/scss/*.scss', ['sass', 'inlinesource']);
+  gulp.watch('dist/**/*.html', ['browser-sync-reload']);
+});
+
+gulp.task('default', ['sass', 'inlinesource', 'watch', 'browser-sync']);
